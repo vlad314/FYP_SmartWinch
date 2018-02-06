4f36c24c55fe27f9279f3abcf83f6339f69851c7
AfdhalAtiffTan <AfdhalAtiffTan@yahoo.com>
2018-02-06 06:34:37 +0000

    added primitive delfino code, only pid works atm.

---
+++ Delfino/.metadata/.plugins/org.eclipse.core.resources/.history/56/e0f51153440a00181312dc1c5496434e
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../eQEP_Calc/eqep_ex1_calculation.c 
+
+C_DEPS += \
+./eQEP_Calc/eqep_ex1_calculation.d 
+
+OBJS += \
+./eQEP_Calc/eqep_ex1_calculation.obj 
+
+OBJS__QUOTED += \
+"eQEP_Calc\eqep_ex1_calculation.obj" 
+
+C_DEPS__QUOTED += \
+"eQEP_Calc\eqep_ex1_calculation.d" 
+
+C_SRCS__QUOTED += \
+"../eQEP_Calc/eqep_ex1_calculation.c" 
+
+
---
+++ Delfino/SmartWinch/CPU1_RAM/eQEP_Calc/eqep_ex1_calculation.d
@@ -0,0 +1,143 @@
+# FIXED
+
+eQEP_Calc/eqep_ex1_calculation.obj: ../eQEP_Calc/eqep_ex1_calculation.c
+eQEP_Calc/eqep_ex1_calculation.obj: ../eQEP_Calc/eqep_ex1_calculation.h
+eQEP_Calc/eqep_ex1_calculation.obj: D:/AcademicRelated/Y4S1/FYP/Codes/Delfino/SmartWinch/device/driverlib.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_memmap.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/adc.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/stdbool.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/stdint.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_adc.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_sysctl.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_types.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/cpu.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/debug.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/asysctl.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_asysctl.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/can.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_can.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/sysctl.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_nmi.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/interrupt.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_ints.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_pie.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/cla.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_cla.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/cmpss.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_cmpss.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/cputimer.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_cputimer.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/dac.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_dac.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/dcsm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_dcsm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/dma.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_dma.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/ecap.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_ecap.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/emif.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_emif.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_memcfg.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/epwm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_epwm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/eqep.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_eqep.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/flash.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_flash.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/gpio.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_gpio.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_xint.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/xbar.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_epwmxbar.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_inputxbar.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_outputxbar.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_xbar.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/hrpwm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_hrpwm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/hrpwm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/i2c.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_i2c.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/mcbsp.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_mcbsp.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/memcfg.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/pin_map.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/sci.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_sci.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/sdfm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_sdfm.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/spi.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_spi.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/upp.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_upp.h
+eQEP_Calc/eqep_ex1_calculation.obj: C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/version.h
+
+../eQEP_Calc/eqep_ex1_calculation.c: 
+../eQEP_Calc/eqep_ex1_calculation.h: 
+D:/AcademicRelated/Y4S1/FYP/Codes/Delfino/SmartWinch/device/driverlib.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_memmap.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/adc.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.6.LTS/include/stdint.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_adc.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_sysctl.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_types.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/cpu.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/debug.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/asysctl.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_asysctl.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/can.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_can.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/sysctl.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_nmi.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/interrupt.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_ints.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_pie.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/cla.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_cla.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/cmpss.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_cmpss.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/cputimer.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_cputimer.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/dac.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_dac.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/dcsm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_dcsm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/dma.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_dma.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/ecap.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_ecap.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/emif.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_emif.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_memcfg.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/epwm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_epwm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/eqep.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_eqep.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/flash.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_flash.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/gpio.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_gpio.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_xint.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/xbar.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_epwmxbar.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_inputxbar.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_outputxbar.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_xbar.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/hrpwm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_hrpwm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/hrpwm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/i2c.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_i2c.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/mcbsp.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_mcbsp.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/memcfg.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/pin_map.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/sci.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_sci.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/sdfm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_sdfm.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/spi.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_spi.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/upp.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/inc/hw_upp.h: 
+C:/ti/c2000/C2000Ware_1_00_03_00/driverlib/f2837xs/driverlib/version.h: 
